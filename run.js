af3d79addf5fbf2e5ffb8c9d6d7f96d725b79034
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2020-02-05 12:38:34 +0100

    demonstrate the problem with modern puppeteer

---
+++ run.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const puppeteer = require('puppeteer');
+
+async function main() {
+	const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false});
+	const page = (await browser.pages())[0];
+
+	page.setContent('<input type=file accepts="aac mp3">');
+}
+
+(async () => {
+    try {
+        await main();
+    } catch (e) {
+        console.error(e.stack);
+        process.exit(2);
+    }
+})();
